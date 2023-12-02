8bee0ede48fd61a81d1b79a42533564c424c0893
Abdusattar2005 <abdaliuuluabdusattar2005@gmail.com>
2023-12-02 22:54:39 +0600

    save fail

--- PHP/first.php
+++ PHP/first.php
@@ -1,10 +1,15 @@
 <?php
 
 $name = "Абдусаттар";
-$array = [0,1, 2, 3, 4, 5, ];
+$hobby=[345,5,356,];
+$array = [0,1, 2, 3, 4, 5,6,7, ];
 foreach ($array as $item)
 
 {
     print $name;
     printf($item."\n");
+}
+foreach ($hobby as $item) {
+    print $hobby;
+    printf($item."\a" );
 }
